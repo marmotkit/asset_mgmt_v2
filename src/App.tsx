7a5a4fa649ebfd6a0c6f2ef104db9966be3dbe79
Golf Management System <user@example.com>
2025-05-07 11:00:12 +0800

    修改路由模式，從 BrowserRouter 改為 HashRouter 以修復部署後的 404 問題

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { HashRouter as Router } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { zhTW } from 'date-fns/locale';
+import zhTW from 'date-fns/locale/zh-TW';
 import { AuthProvider } from './contexts/AuthContext';
 import theme from './theme';
 import { storageService } from './services/storageService';
 import AppRoutes from './routes';
+import { SnackbarProvider } from 'notistack';
 
 const App: React.FC = () => {
   useEffect(() => {
@@ -22,7 +22,9 @@ const App: React.FC = () => {
         <CssBaseline />
         <AuthProvider>
           <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={zhTW}>
-            <AppRoutes />
+            <SnackbarProvider maxSnack={3}>
+              <AppRoutes />
+            </SnackbarProvider>
           </LocalizationProvider>
         </AuthProvider>
       </ThemeProvider>
